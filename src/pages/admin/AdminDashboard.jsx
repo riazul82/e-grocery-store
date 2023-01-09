5a2e5558a482ce4586ed43f84b07618aff83f709
riazul islam <riazulislam5280@gmail.com>
2023-01-09 11:37:36 +0600

    admin dashboard page updated

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -4,10 +4,32 @@ import AdminSidebar from '../../components/admin/AdminSidebar';
 import AdminOrdersList from '../../components/admin/AdminOrdersList';
 import AppLayout from '../../layouts/AppLayout';
 
+import useAdminUsersList from '../../hooks/useAdminUsersList';
 import useAdminOrdersList from '../../hooks/useAdminOrdersList';
 
 const AdminDashboard = () => {
     const ordersList = useAdminOrdersList();
+    const usersList = useAdminUsersList();
+
+    let totalUsers = usersList.length;
+    let totalOrders = 0;
+    let pendingOrders = 0;
+    let shippedOrders = 0;
+
+    for (let i = 0; i < ordersList.length; i ++) {
+        if (ordersList[i].status !== 'canceled') {
+            totalOrders ++;
+        }
+
+        if (ordersList[i].status === 'pending') {
+            pendingOrders ++;
+        }
+
+        if (ordersList[i].status === 'delivered') {
+            shippedOrders ++;
+        }
+    }
+
 
     return (
         <AppLayout>
@@ -21,23 +43,20 @@ const AdminDashboard = () => {
 
                     <div className="dashboardCards">
                         <div className="dahsboardCard">
-                            {/* <p className="cardCounter">{orderList.length < 10 ? `0${orderList.length}` : orderList.length}</p> */}
-                            <p className="cardCounter">00</p>
+                            <p className="cardCounter">{totalUsers < 10 ? `0${totalUsers}` : totalUsers}</p>
                             <p className="cardText">Total Users</p>
                         </div>
                         <div className="dahsboardCard">
-                            {/* <p className="cardCounter">{orderList.length < 10 ? `0${orderList.length}` : orderList.length}</p> */}
-                            <p className="cardCounter">00</p>
+                            <p className="cardCounter">{totalOrders < 10 ? `0${totalOrders}` : totalOrders}</p>
                             <p className="cardText">Total Orders</p>
                         </div>
                         <div className="dahsboardCard">
-                            <p className="cardCounter">00</p>
+                            <p className="cardCounter">{pendingOrders < 10 ? `0${pendingOrders}` : pendingOrders}</p>
                             <p className="cardText">Pending Orders</p>
                         </div>
                         <div className="dahsboardCard">
-                            {/* <p className="cardCounter">{orderList.length < 10 ? `0${orderList.length}` : orderList.length}</p> */}
-                            <p className="cardCounter">00</p>
-                            <p className="cardText">Product Sales</p>
+                            <p className="cardCounter">{shippedOrders < 10 ? `0${shippedOrders}` : shippedOrders}</p>
+                            <p className="cardText">Shipped Orders</p>
                         </div>
                     </div>
 
