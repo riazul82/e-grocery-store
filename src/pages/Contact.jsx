9cc5b6c49c4e678663e8db039e5e6da7c4eaaec8
riazul islam <riazulislam5280@gmail.com>
2023-01-22 10:46:21 +0600

    contact page responsive completed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,49 +12,49 @@ const Contact = () => {
             <div className="contact">
                 <Title title="Contact" desc="Any Question? Just write us a message" />
                 <div className="contactWrap">
-                    <div class="contactAddress">
-                        <div class="contactAddressText">
+                    <div className="contactAddress">
+                        <div className="contactAddressText">
                             <h1 className="contactTextHeading">Let's Connect!</h1>
                             <p className="contactTextDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit dolor nihil iste porro distinctio. In
                                 harum quidem mollitia quas natus aliquam dolore dolores.</p>
                         </div>
-                        <div class="contactInfo">
-                            <div class="contactInfoBox officeAddress">
+                        <div className="contactInfo">
+                            <div className="contactInfoBox officeAddress">
                                 <h3><FaMapMarkedAlt className="Icon" /><span>Address</span></h3>
                                 <p>North Tower(3rd floor), Dhaka, Bangladesh</p>
                             </div>
 
-                            <div class="contactInfoBox contactMail">
+                            <div className="contactInfoBox contactMail">
                                 <h3><IoMdMail className="Icon" /><span>Mail</span></h3>
                                 <p>ebazar@example.com</p>
                             </div>
 
-                            <div class="contactInfoBox contactPhone">
+                            <div className="contactInfoBox contactPhone">
                                 <h3><FaHeadset className="Icon" /><span>Support</span></h3>
                                 <p>+88 0123456789, +88 01298765431</p>
                             </div>
                         </div>
                     </div>
 
-                    <div class="contactForm">
+                    <div className="contactForm">
                         <form action="#">
-                            <div class="inputField">
+                            <div className="inputField">
                                 <input type="text" placeholder="Your name" autocomplete="off" required />
                             </div>
 
-                            <div class="inputField">
+                            <div className="inputField">
                                 <input type="email" placeholder="Your email" autocomplete="off" required />
                             </div>
 
-                            <div class="inputField">
+                            <div className="inputField">
                                 <input type="number" placeholder="Phone number" autocomplete="off" required />
                             </div>
 
-                            <div class="textareaField">
+                            <div className="textareaField">
                                 <textarea name="textarea" id="textarea" placeholder="Your message" required></textarea>
                             </div>
 
-                            <div class="buttonField">
+                            <div className="buttonField">
                                 <button type="submit">Send</button>
                             </div>
                         </form>
